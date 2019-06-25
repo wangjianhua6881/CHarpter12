1c3114c32940705abc7557c5f3c1e532a758411c
wangjianhua6881 <15235088663@163.com>
2019-06-25 16:18:25 +0800

    配置文件修改为阿里云

--- charpter12/src/main/java/com/course/cases/GetUserListInfoTest.java
+++ charpter12/src/main/java/com/course/cases/GetUserListInfoTest.java
@@ -21,25 +21,36 @@ public class GetUserListInfoTest {
 
     @Test(dependsOnGroups = "loginTrue",description = "获取用户信息列表接口测试")
     public void getUserListInfo() throws IOException {
+
+        //获取用来执行sql的SqlSession
         SqlSession session = DatabaseUtil.getSqlSession();
+
+        //查询自己数据库中的测试用例数据
         GetUserList getUserList = session.selectOne("getUserInfoListCase",1);
+
+        //输出查询到的结果和将要访问的路由
         System.out.println(getUserList.toString());
         System.out.println(TestConfig.getUserListUrl);
 
+        //实际结果（获取请求中接口返回的数据 参数 用例数据&接口路由地址）
         JSONArray getArray = getJSONArray(getUserList,TestConfig.getUserListUrl);
-        List<User> userList = session.selectList("getUserInfoList",getUserList);
 
+        //查询预期结果，并将获取到的user列表循环输出
+        List<User> userList = session.selectList("getUserInfoList",getUserList);
         for (User u:userList) {
             System.out.println("获取到的用户信息：" + u);
         }
-
+        //将预期转换为JSONObject
         JSONArray sessionList = new JSONArray(userList);
 
+        //对比预期结果与实际结果的字符长度
         Assert.assertEquals(sessionList.length(),getArray.length());
 
+        //将预期结果&实际结果转换为字符串
         String array1 = String.valueOf(sessionList);
         String array2 = String.valueOf(getArray);
 
+        //对比字符串内容是否一致
         try{
             if (array1.equals(array2)){
                 System.out.println("对比成功！");
@@ -60,6 +71,8 @@ public class GetUserListInfoTest {
     }
 
     private JSONArray getJSONArray(GetUserList userList,String url) throws IOException {
+
+        //新建JSON对象，并设置值
         JSONObject json = new JSONObject();
         json.put("id",userList.getId());
         json.put("userName",userList.getUserName());
@@ -67,7 +80,9 @@ public class GetUserListInfoTest {
         json.put("sex",userList.getSex());
         json.put("permission",userList.getPermission());
 
+        //新建post请求，并将参数路由加进去
         HttpPost post = new HttpPost(url);
+        //
         StringEntity entit = new StringEntity(json.toString(),"utf-8");
         post.setEntity(entit);
         post.setHeader("content-type","application/json");
